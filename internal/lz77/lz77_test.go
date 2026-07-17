ec7a758e86c1b5b805422ef0354215a0faddcee9
restorte <vanyakarpenko837@yandex.ru>
2026-07-17 21:42:43 +0300

    add more tests for compress

--- internal/lz77/lz77_test.go
+++ internal/lz77/lz77_test.go
@@ -13,6 +13,9 @@ func TestCompressV0(t *testing.T) {
 	}{
 		{name: "empty", in: []byte{}, want: []byte{}},
 		{name: "one byte", in: []byte("a"), want: []byte{0, 'a'}},
+		{name: "eight bytes", in: []byte("abcdefgh"), want: []byte{0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}},
+		{name: "nine bytes", in: []byte("abcdefghi"), want: []byte{0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 0, 'i'}},
+		{name: "abcabcabc", in: []byte("abcabcabc"), want: []byte{0, 'a', 'b', 'c', 'a', 'b', 'c', 'a', 'b', 0, 'c'}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
